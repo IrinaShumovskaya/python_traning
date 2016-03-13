e8153080b100d855bc8233cd1efc1c1b92944075
IrinaShumovskaya <soltana22@ya.ru>
2016-03-13 21:45:00 +0300

    Создание нового контакта

---
+++ contact.py
@@ -0,0 +1,23 @@
+class Contact:
+    def __init__(self, firstname, middlename, lastname, nickname, title, company, adress, home_phone,
+                       mobile_phone, work_phone, fax, email2, email3, homepage, birthday_year, anniversary_year,
+                       adress2, home_phone2, notes):
+        self.firstname = firstname
+        self.middlename = middlename
+        self.lastname = lastname
+        self.nickname = nickname
+        self.title = title
+        self.company = company
+        self.adress = adress
+        self.home_phone = home_phone
+        self.mobile_phone = mobile_phone
+        self.work_phone = work_phone
+        self.fax = fax
+        self.email2 = email2
+        self.email3 = email3
+        self.homepage = homepage
+        self.birthday_year = birthday_year
+        self.anniversary_year = anniversary_year
+        self.adress2 = adress2
+        self.home_phone2 = home_phone2
+        self.notes = notes
