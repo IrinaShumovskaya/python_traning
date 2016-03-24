f5506d0025961d1d3b42e37d7865d87e28dc2875
IrinaShumovskaya <soltana22@ya.ru>
2016-03-24 13:11:02 +0300

    Выполнена оптимизация ввода данных в поля группы

--- test/test_edit_group.py
+++
@@ -1,7 +0,0 @@
-from model.group import Group
-
-
-def test_edit_group(app):
-    app.session.login(username="admin", password="secret")
-    app.group.edit_first_group(Group(name="edit-name", header="edit-header", footer="edit-footer"))
-    app.session.logout()
